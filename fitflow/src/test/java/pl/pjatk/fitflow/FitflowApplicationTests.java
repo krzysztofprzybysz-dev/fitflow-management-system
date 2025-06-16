222c1e5a364025503e921ec46f205d9f277b26af
Krzysztof Przybysz <95767858+krzysztofprzybysz-dev@users.noreply.github.com>
2025-06-16 09:13:58 +0200

    project init

---
+++ fitflow/src/test/java/pl/pjatk/fitflow/FitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.fitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
